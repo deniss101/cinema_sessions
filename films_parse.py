77b1d1786397463c2e60f0d439080242323bf811
deniss101 <96088577+deniss101@users.noreply.github.com>
2022-06-14 20:28:34 +0300

    Taking film list, sessions and prices
    Correction of data from url fuction. Correct getting sessions and prices

--- films_parse.py
+++ films_parse.py
@@ -1,37 +1,44 @@
 import requests
 from bs4 import BeautifulSoup as BS
-from datetime import datetime
+from datetime import date, datetime, timezone
 
-film_list, link_list, cinema_list, sessions_list, = [], [], [], []
+film_list, link_list = [], []
 
 
 def get_data_from_url(url):
     request = requests.get(url)
-    html_content = BS(request.content, 'lxml')
-    return html_content
+    return request
 
 
 def get_film_list():
     url = "https://kudago.com/msk/kino/schedule-cinema/"
-    html_data = get_data_from_url(url)
+    html_data = BS(get_data_from_url(url).content, 'lxml')
     films = html_data.find_all(class_='post-title-link')
-    print(films)
     for film in films:
         film_name = film.get('title')
         film_link = film.get('href')
         film_list.append(film_name)
         link_list.append(film_link)
+        print(film_name +' '+ film_link)
 
 
-def get_cinema_and_time():
-    for url in link_list:
-        html_data = get_data_from_url(url)
-        sessions = html_data.find_all(class_='schedule-showing-row')
-        print(sessions)
-        '''for session in sessions:
-            cinemas = session.get('href')
-            cinema_list.append(cinemas)
-            print(cinema_list)'''
+def get_sessions_price():
+    today = date.today()
+    movie_id = 5335
+    location = 'msk'
+    url = f'https://kudago.com/widgets/movie-showings/?date={today}&movie_id={movie_id}&group_by=place&location={location}'
+    json_file = get_data_from_url(url)
+    json_data = [*json_file.json()['places']]
+    for i in range(len(json_data)):
+        title = json_data[i]['title'].capitalize()
+        address = json_data[i]['address']
+        subway = json_data[i]['subway']
+        times_prices_extract = json_data[i]['showings']
+        times_prices = (times_prices_extract[0])
+        time = (datetime.fromtimestamp(times_prices['time']).strftime('%H:%M'))
+        price = (times_prices['price'])
+        print(f'{title} \nАдрес: {address} \nМетро: {subway}\nСеансы: {time}\nЦена: {price}\n')
+
 
 get_film_list()
-get_cinema_and_time()
+get_sessions_price()
