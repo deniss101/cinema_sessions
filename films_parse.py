bd6f2b62f63c2b5e97d156dca253e3028ff57539
deniss101 <96088577+deniss101@users.noreply.github.com>
2022-06-19 23:11:41 +0300

    Get mulitple session
    Getting data for every film session with time and price.

--- films_parse.py
+++ films_parse.py
@@ -1,8 +1,10 @@
 import requests
-from bs4 import BeautifulSoup as BS
-from datetime import date, datetime, timezone
+import json
+from bs4 import BeautifulSoup as Bs
+from datetime import date, datetime
 
-film_list, link_list = [], []
+TODAY = date.today()
+title_list, link_list, id_list = [], [], []
 
 
 def get_data_from_url(url):
@@ -10,35 +12,39 @@ def get_data_from_url(url):
     return request
 
 
-def get_film_list():
-    url = "https://kudago.com/msk/kino/schedule-cinema/"
-    html_data = BS(get_data_from_url(url).content, 'lxml')
+def get_film_list(location='msk'):
+    url = f'https://kudago.com/{location}/kino/schedule-cinema/'
+    html_data = Bs(get_data_from_url(url).text, 'lxml')
+    ids_extend = html_data.find_all(class_='post post-rect post-editors-choice')
+    for i in ids_extend:
+        film_id_ex = i.get('data-ping-item-id')
+        id_list.append(film_id_ex)
+    ids = html_data.find_all(class_='post post-rect')
+    for i in ids:
+        film_id = i.get('data-ping-item-id')
+        id_list.append(film_id)
     films = html_data.find_all(class_='post-title-link')
     for film in films:
         film_name = film.get('title')
         film_link = film.get('href')
-        film_list.append(film_name)
+        title_list.append(film_name)
         link_list.append(film_link)
-        print(film_name +' '+ film_link)
 
 
-def get_sessions_price():
-    today = date.today()
-    movie_id = 5335
-    location = 'msk'
-    url = f'https://kudago.com/widgets/movie-showings/?date={today}&movie_id={movie_id}&group_by=place&location={location}'
-    json_file = get_data_from_url(url)
-    json_data = [*json_file.json()['places']]
+def get_sessions_price(movie_id=5364, date=TODAY, location='msk'):
+    url = f'https://kudago.com/widgets/movie-showings/?date={date}&movie_id={movie_id}&group_by=place&location={location}'
+    json_file = get_data_from_url(url).text
+    json_data = json.loads(json_file)['places']
     for i in range(len(json_data)):
         title = json_data[i]['title'].capitalize()
         address = json_data[i]['address']
         subway = json_data[i]['subway']
-        times_prices_extract = json_data[i]['showings']
-        times_prices = (times_prices_extract[0])
-        time = (datetime.fromtimestamp(times_prices['time']).strftime('%H:%M'))
-        price = (times_prices['price'])
-        print(f'{title} \nАдрес: {address} \nМетро: {subway}\nСеансы: {time}\nЦена: {price}\n')
+        print(title, address, subway)
+        for show in json_data[i]['showings']:
+            print(datetime.fromtimestamp(show['time']).strftime('%H:%M'), "Цена:", show['price'])
+        print()
 
 
 get_film_list()
 get_sessions_price()
+
