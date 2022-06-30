f978e672d239fec535b80b76ea5bd231ba0bd6f8
deniss101 <96088577+deniss101@users.noreply.github.com>
2022-06-30 21:58:15 +0300

    Save data to sqlbase
    Changed logic of getting data . First step at saving to base.

--- films_parse.py
+++ films_parse.py
@@ -1,24 +1,31 @@
 import requests
 import json
+import sqlite3
 from bs4 import BeautifulSoup as Bs
 from datetime import date, datetime
 
-TODAY = date.today()
-title_list, link_list, id_list = [], [], []
-
+set_date = date.today()
+title_list, id_list = [], []
+con = sqlite3.connect('cinema_sessions.db')
+cur = con.cursor()
 
 def get_data_from_url(url):
     request = requests.get(url)
     return request
 
 
+def sql_operations(query):
+    con = sqlite3.connect('cinema_sessions.db')
+    cur = con.cursor()
+    cur.execute(query)
+    con.commit()
+    cur.close()
+    con.close()
+
+
 def get_film_list(location='msk'):
     url = f'https://kudago.com/{location}/kino/schedule-cinema/'
     html_data = Bs(get_data_from_url(url).text, 'lxml')
-    ids_extend = html_data.find_all(class_='post post-rect post-editors-choice')
-    for i in ids_extend:
-        film_id_ex = i.get('data-ping-item-id')
-        id_list.append(film_id_ex)
     ids = html_data.find_all(class_='post post-rect')
     for i in ids:
         film_id = i.get('data-ping-item-id')
@@ -26,13 +33,16 @@ def get_film_list(location='msk'):
     films = html_data.find_all(class_='post-title-link')
     for film in films:
         film_name = film.get('title')
-        film_link = film.get('href')
         title_list.append(film_name)
-        link_list.append(film_link)
+    for i in range(len(id_list)):
+        print(title_list[i+1])          #title_list[i+1], to remove with useless film with id=1
+        cur.execute("INSERT INTO tab (title) VALUES(?)", (str(1)))
+        get_sessions_price(id_list[i])
 
 
-def get_sessions_price(movie_id=5364, date=TODAY, location='msk'):
-    url = f'https://kudago.com/widgets/movie-showings/?date={date}&movie_id={movie_id}&group_by=place&location={location}'
+
+def get_sessions_price(movie_id, location='msk'):
+    url = f'https://kudago.com/widgets/movie-showings/?date={set_date}&movie_id={movie_id}&group_by=place&location={location}'
     json_file = get_data_from_url(url).text
     json_data = json.loads(json_file)['places']
     for i in range(len(json_data)):
@@ -41,10 +51,14 @@ def get_sessions_price(movie_id=5364, date=TODAY, location='msk'):
         subway = json_data[i]['subway']
         print(title, address, subway)
         for show in json_data[i]['showings']:
-            print(datetime.fromtimestamp(show['time']).strftime('%H:%M'), "Цена:", show['price'])
-        print()
+            print(datetime.fromtimestamp(show['time']).strftime('%H:%M'), "Цена:", show['price'], '\n')
+            time = datetime.fromtimestamp(show['time']).strftime('%H:%M')
+            price = show['price']
+            cur.execute("INSERT INTO tab (cinema, address, subway, time, price) VALUES(?, ?, ?, ?, ?)", (title, address, subway, time, price))
 
 
 get_film_list()
-get_sessions_price()
+con.commit()
+cur.close()
+con.close()
 
